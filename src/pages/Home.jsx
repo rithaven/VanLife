7f89029de32337acaa56d6f0e4480bfb3d7c4621
rithaven <veneruth4@gmail.com>
2023-05-15 15:39:36 +0200

    Add Top buttons/Vans.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import HeroImg from "../images/home-hero.png";
 export default function Home() {
   return (
     <div className="relative h-screen">
-      <img src={HeroImg} className="object-cover w-full h-screen" />
+      <img src={HeroImg} className="object-cover w-full h-screen" alt="vans" />
       <div className="absolute text-white left-10 right-10 lg:left-40 lg:top-60 lg:right-40 top-60">
         <h1 className="mb-10 text-3xl font-bold lg:mb-8 lg:text-7xl">
           You got the travel plans, we got the travel vans.
