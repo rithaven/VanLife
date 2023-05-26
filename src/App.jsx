04cf1fa59f28490f1ca6bbf6e0b9238a0f4cde47
Kankindi Ritha Veneranda <52707943+rithaven@users.noreply.github.com>
2023-05-26 16:40:00 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,10 @@ function App() {
             <Route index element={<Vans />} />
             <Route path=":id" element={<VansDetail />} />
           </Route>
-          <Route path="/host" element={<HostLayout />}>
+          <Route path="host" element={<HostLayout />}>
             <Route index element={<Dashboard />} />
-            <Route path="host/income" element={<Income />} />
-            <Route path="host/reviews" element={<Reviews />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
         </Route>
       </Routes>
